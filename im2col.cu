23d1ba356481c83981fba902b4cd97c96776e3c2
stoneforestwhu <stoneforestwhu@gmail.com>
2019-12-02 20:43:39 +0800

    Update im2col.cu

--- im2col.cu
+++ im2col.cu
@@ -1,15 +1,17 @@
+//  __global__ 表示共享内存，并行调用的关键字，gpu核函数必须以此声明
+//  n 代表conv kernel的个数，n=Cin*H*W
+//  DType*  data_im 代表一个张量，为输入图像(Cin,H,W)，以_im为后缀的都与输入图像相关
+//  feature map的高宽和kernel的高宽
 template <typename DType>
-__global__ void im2col_gpu_kernel(const int n, const DType* data_im, // __global__ 表示共享内存，并行调用的关键字，gpu核函数必须以此声明
-                                                                     // n 代表conv kernel的个数，n=Cin*H*W
-                                                                     // DType*  data_im 代表一个张量，为输入图像(Cin,H,W)，以_im为后缀的都与输入图像相关
-    const int height, const int width, const int kernel_h, const int kernel_w, //feature map的高宽和kernel的高宽
-    const int pad_h, const int pad_w,
-    const int stride_h, const int stride_w,
-    const int dilation_h, const int dilation_w,
-    const int height_col, const int width_col, // 输出column的高宽
-    DType* data_col) 
-{ // 输出column张量，(Kh*Kw*Cin,H,W)
-
+__global__ void im2col_gpu_kernel(const int n, 
+                                  const DType* data_im,                                                                                                             const int height, const int width,             //  feature map的高宽
+                                  const int kernel_h, const int kernel_w,        //  kernel的高宽
+                                  const int pad_h, const int pad_w,                  
+                                  const int stride_h, const int stride_w,            
+                                  const int dilation_h, const int dilation_w,        
+                                  const int height_col, const int width_col,     //  输出column的高宽
+                                  DType* data_col)                               //  输出column张量，(Kh * Kw* Cin, H, W)
+{     
     for(int index = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x){ 
         //  在0-gridDim.x*blockDim.x之间并行，到n结束，index代表第i个conv kernel的索引
         //  index index of output matrix
@@ -26,7 +28,7 @@ __global__ void im2col_gpu_kernel(const int n, const DType* data_im, // __global
         DType* data_col_ptr = data_col;  //获得输出张量的指针拷贝
         // 指针向前移动，由于index是0-Cin*H*W，c_col,h_col和w_col有Cin、H和W种取值，正好对应index
         data_col_ptr += (c_col * height_col + h_col) * width_col + w_col; 
-        const DType* data_im_ptr = data_im; //获取输入张量的指针拷贝
+        const DType* data_im_ptr = data_im; //  获取输入张量的指针拷贝
         data_im_ptr += (c_im * height + h_offset) * width + w_offset; //指针向前移动
         for (int i = 0; i < kernel_h; ++i) {
             for (int j = 0; j < kernel_w; ++j) { // 对单个kernel进行循环
